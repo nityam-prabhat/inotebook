77358925b609e4a2c206e0b4d763c5d7be81defc
Nityam Prabhat <nprabhat2002@gmail.com>
2024-04-25 17:10:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import NoteState from './context/notes/NoteState';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Alert from './components/Alert';
+import { useState } from 'react';
+import ErrorPage from './components/ErrorPage';
 
 function App() {
+  const [alert, setAlert] = useState(null)
+  const setAlertValues = (type, message) =>{
+    setAlert({type:type, message:message});
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NoteState>
+        <Router>
+          <Navbar setAlert={setAlertValues}/>
+          {alert && <Alert alert={alert}/>}
+          <div className='container'>
+            <Routes>
+              <Route path="/" element={<Home setAlert={setAlertValues}/>} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login setAlert={setAlertValues}/>} />
+              <Route path="/signup" element={<Signup setAlert={setAlertValues}/>} />
+              <Route path='*' element={<ErrorPage/>} />
+            </Routes>
+          </div>
+        </Router>
+      </NoteState>
     </div>
   );
 }
